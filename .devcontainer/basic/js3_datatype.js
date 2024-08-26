16757cfa675550afefbd7da58027ba3c00d6c3d8
Mohammad Athar <118742167+Athar2000@users.noreply.github.com>
2024-08-26 10:32:26 +0000

    container config done

---
+++ .devcontainer/basic/js3_datatype.js
@@ -0,0 +1,16 @@
+//number=>2 to power 53
+//bigint
+//String=>""
+//boolean=>true or false
+//null=>standalone value 
+//undefined
+//symbol=>unique
+//object
+
+
+let name="Athar bhai"
+let age="18"
+let isTRUE=false;
+let state;
+console.table([name,age,isTRUE,state])
+console.log(typeof "AThar bhai")
---
+++ .devcontainer/basic/js_variable.js
@@ -0,0 +1,11 @@
+const accountId=1443
+let accountEmail="AThar@gmail.com"
+var accoundPassword="Athar@15"
+accountCity="Lucknow"
+//accountId=2//not alllowed
+accountEmail="Anwar@gmail.com"
+accoundPassword="Anwar@14"
+accountCity="Agra"
+let accountState;
+console.table([accoundPassword,accountEmail,accountId,accountCity,accountState])
+
